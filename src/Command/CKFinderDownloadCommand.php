7f62fe37372454c6446a58e9eca2f9e83c1155d7
bmlotek <b.mlotek@cksource.com>
2024-04-16 12:58:20 +0200

    3.7.0

--- src/Command/CKFinderDownloadCommand.php
+++ src/Command/CKFinderDownloadCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpKernel\Kernel;
 
 class CKFinderDownloadCommand extends Command
 {
-    const LATEST_VERSION = '4.0.0';
+    const LATEST_VERSION = '3.7.0';
 
     protected $name = 'ckfinder:download';
 
